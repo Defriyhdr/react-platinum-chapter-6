662542735fa98f3248f3d4892cf2afc6f91e1360
Defriyhdr <Defiyhdr@gmail.com>
2023-12-01 20:08:00 +0700

    add directory hoc

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Detail from "./pages/Detail";
-import Login from "./pages/Login";
-import NewMenu from "./pages/NewMenu";
-import Edit from "./pages/Edit";
+import { routes } from "./routes";
+import { useRoutes } from "react-router-dom";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new-menu" element={<NewMenu />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/edit/:id" element={<Edit />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  let element = useRoutes(routes);
+  return element;
 };
 
 export default App;
