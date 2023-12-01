1f98fcb6941f2a87a6565bb77e7074ab53573564
Defriyhdr <Defiyhdr@gmail.com>
2023-12-01 21:49:19 +0700

    add routes component

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,26 +3,48 @@ import NewMenu from "./pages/NewMenu";
 import Login from "./pages/Login";
 import Detail from "./pages/Detail";
 import Edit from "./pages/Edit";
+import ProtectedRoute from "./hoc/ProtectedRoute";
+import AuthRoute from "./hoc/AuthRoute";
 
 export const routes = [
   {
     path: "/",
-    element: <Home />,
+    element: (
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/new-menu",
-    element: <NewMenu />,
+    element: (
+      <ProtectedRoute>
+        <NewMenu />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/login",
-    element: <Login />,
+    element: (
+      <AuthRoute>
+        <Login />
+      </AuthRoute>
+    ),
   },
   {
     path: "/detail/:id",
-    element: <Detail />,
+    element: (
+      <ProtectedRoute>
+        <Detail />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/edit/:id",
-    element: <Edit />,
+    element: (
+      <ProtectedRoute>
+        <Edit />
+      </ProtectedRoute>
+    ),
   },
 ];
